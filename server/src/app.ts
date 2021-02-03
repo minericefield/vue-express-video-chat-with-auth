cf35c76802aa809b7e16be374c2420add3dcfa82
minericefield <minericefield@gmail.com>
2021-02-04 01:21:25 +0900

    define staticdir

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
+
+const staticDir = path.resolve(__dirname, 'public')
+app.use(express.static(staticDir))
 
 export default app
