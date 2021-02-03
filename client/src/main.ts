18dc66a6bafce3cae23448c33dc90c230175508e
minericefield <minericefield@gmail.com>
2021-02-04 03:03:56 +0900

    init bootstrap

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-import '@/assets/styles/index.scss'
+import './assets/styles/index.scss'
+
+import './plugins/bootstrap'
 
 createApp(App).use(store).use(router).mount('#app')
