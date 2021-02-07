6632e96d5a7807a917f3bc0f1da33e5fc10150e4
minericefield <minericefield@gmail.com>
2021-02-07 16:23:31 +0900

    add routes in view for development temporaly

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <router-view />
+  <div>
+    <div>
+      <router-link to="/">Top</router-link> |
+      <router-link to="/communication/hash">Communication</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Regsiter</router-link> |
+      <router-link to="/verify/hash">Vefiry</router-link> |
+      <router-link to="/me">Me</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
