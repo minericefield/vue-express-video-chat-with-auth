45de26f9b5a5755c5c1e9b9e30094245ce1cadc1
minericefield <minericefield@gmail.com>
2021-02-07 16:53:58 +0900

    update routePlugin

--- client/src/plugins/checkAuthWhenRouteLeave.ts
+++ client/src/plugins/checkAuthWhenRouteLeave.ts
@@ -3,11 +3,13 @@ import { Router } from 'vue-router'
 import AuthApi from '../api/Auth'
 
 export default (router: Router) => {
-  router.beforeEach(async (to, _, next) => {
+  router.beforeEach(async (to, from, next) => {
+    if (to.name === from.name) return
+
     if (to.meta.needsAuth) {
       const result = await new AuthApi().isUserActive()
       if (!result.succeed) {
-        return router.push({
+        await router.push({
           name: 'AuthLogin'
         })
       }
