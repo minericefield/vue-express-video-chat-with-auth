22908b93a8acabe2a4bc7f0a47094fa0af7b82d5
minericefield <minericefield@gmail.com>
2021-02-07 16:34:51 +0900

    fix auth api to edit config

--- client/src/api/Auth.ts
+++ client/src/api/Auth.ts
@@ -1,10 +1,12 @@
+import { AxiosRequestConfig} from 'axios'
+
 import { RegistrationRequest } from '../types/swagger'
 
 import BaseApi from './Base'
 
 export default class AuthApi extends BaseApi {
-  constructor() {
-    super('auth')
+  constructor(config?: AxiosRequestConfig) {
+    super('auth', config)
   }
 
   public register (registrationRequest: RegistrationRequest) {
