029fdadf199628179aa335949a349e716e5d0a64
minericefield <minericefield@gmail.com>
2021-02-09 01:26:35 +0900

    add layout to me

--- client/src/views/AuthMe.vue
+++ client/src/views/AuthMe.vue
@@ -1,3 +1,20 @@
 <template>
-  <div class="auth-me" />
+  <layout-default>
+    <div class="aute-me">
+      プロフィール
+    </div>
+  </layout-default>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import LayoutDefault from '../layouts/LayoutDefault.vue'
+
+export default defineComponent({
+  name: 'AuthMe',
+  components: {
+    LayoutDefault
+  }
+})
+</script>
