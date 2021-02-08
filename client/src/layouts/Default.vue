f54c9440e2e56e3174a058b9ac863720a5a3e845
minericefield <minericefield@gmail.com>
2021-02-08 23:46:05 +0900

    add an empty default layout

---
+++ client/src/layouts/Default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="layout-default d-flex flex-column position-relative w-100 h-100">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    </nav>
+    <div class="main">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+</script>
