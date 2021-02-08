0cab0a1fb3a04c0ea0878d332fda457389772c49
minericefield <minericefield@gmail.com>
2021-02-08 22:56:37 +0900

    add login to controller

--- server/src/controllers/Auth.ts
+++ server/src/controllers/Auth.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import urlJoin from 'url-join'
 
 import { UserDoc } from '../models/User'
@@ -15,10 +16,9 @@ export default class AuthController extends UserController {
     return new Promise ((resolve, reject) => {
       this.model.findOne({ _id: userId, status: 'active' }, null, null, (error, userDoc) => {    
         if (error || !userDoc) {
-          console.log({ error })
           return reject(error)
         }
-    
+
         resolve(userDoc)
       })
     })
@@ -27,13 +27,13 @@ export default class AuthController extends UserController {
   public async register (user: Partial<UserDoc>) {
     const userDoc = await this.create(user)
     await this.sendVerifyEmail(userDoc)
+
     return userDoc
   }
 
   private async sendVerifyEmail (userDoc: UserDoc) {
     const encryptedUserId = simpleTextEncrypt(userDoc._id.toString())
     const content = `<a href="${urlJoin(process.env.HOST, 'verify', encryptedUserId)}"> Please verify your account from this link. </a>`
-
     await sendEmail(userDoc.email, 'Thank you for register!', content)
 
     return userDoc
@@ -41,6 +41,25 @@ export default class AuthController extends UserController {
 
   public async verify (userHash: string) {
     const userId = await this.update(simpleTextDecrypt(userHash), { status: 'active' })
+
     return userId
+  }
+
+  public login ({ email, password }: { email: string, password: string }): Promise<UserDoc> {
+    return new Promise ((resolve, reject) => {
+      this.model.findOne({ email, status: 'active' }, null, null, (error, userDoc) => {    
+        if (error || !userDoc) {
+          return reject(error)
+        }
+
+        bcrypt.compare(password, userDoc.password, (error, isSame) =>{
+          if(isSame){ 
+            resolve(userDoc)
+          } else {
+            reject(error)
+          }
+        })
+      })
+    })
   }
 }
