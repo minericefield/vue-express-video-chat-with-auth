674bd6742f3192e8a30c47e0cdec7e98317f8296
minericefield <minericefield@gmail.com>
2021-02-10 01:22:39 +0900

    transit to communication

--- client/src/views/Top.vue
+++ client/src/views/Top.vue
@@ -1,20 +1,53 @@
 <template>
   <layout-default>
-    <div class="top">
-      トップ
-    </div>
+    <nav class="navbar navbar-expand-lg">
+      <button
+        @click="isCreateChannelModalVisible = true"
+        class="btn btn-primary"
+      >
+        Start Video
+      </button>
+    </nav>
+
+    <teleport to="#modal-overlay">
+      <create-channel-modal
+        v-if="isCreateChannelModalVisible"
+        @on-channel-defined="joinChannel"
+        @on-left="isCreateChannelModalVisible = false"
+      />
+    </teleport>
   </layout-default>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 import LayoutDefault from '../layouts/LayoutDefault.vue'
+import CreateChannelModal from '../components/CreateChannelModal.vue'
 
 export default defineComponent({
   name: 'Top',
   components: {
-    LayoutDefault
+    LayoutDefault,
+    CreateChannelModal
+  },
+  setup () {
+    const isCreateChannelModalVisible = ref(false)
+    const ruter = useRouter()
+
+    // TODO: request server and get hash form server
+    const joinChannel = (channelName: string) => {
+      isCreateChannelModalVisible.value = false
+      ruter.push({ name: 'Communication', params: { communicationHash: channelName } })
+      // useRouter().push({ name: 'Communication', params: { communicationHash: channelName } })
+    }
+
+    return {
+      isCreateChannelModalVisible,
+
+      joinChannel
+    }
   }
 })
 </script>
