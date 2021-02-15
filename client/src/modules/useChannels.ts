ee2ba58a35e92929421e4b9a5b455ad2fdd7fc7e
minericefield <minericefield@gmail.com>
2021-02-15 20:37:34 +0900

    add channel module

---
+++ client/src/modules/useChannels.ts
@@ -0,0 +1,61 @@
+import { io } from 'socket.io-client'
+import { ToRefs, InjectionKey, ref, computed, unref } from 'vue'
+
+import { Me } from './useAuthMe'
+import { State as VideoSettingsState } from './useVideoSettings'
+import { ChannelMember } from '../types/ChannelMember'
+
+type Channel = {
+  name: string;
+  members: ChannelMember[];
+}
+
+const socket = io(process.env.VUE_APP_HOST, {
+  transports: ['websocket', 'polling', 'flashsocket'],
+})
+
+export const useChannels = (me: ToRefs<Pick<Me, "_id" | "name">>, videoSettingsState: ToRefs<VideoSettingsState>) => {
+  const channels = ref<Channel[]>([])
+  const meAsMember = computed<ChannelMember>(() => {
+    return {
+      isAudioOn: videoSettingsState.isAudioOn.value,
+      isVideoOn: videoSettingsState.isVideoOn.value,
+      _id: me._id.value,
+      name: me.name.value
+    }
+  })
+  const joiningChannel = computed(() => {
+    return (name: string) => {
+      return channels.value.find((channel) => channel.name === name)
+    }
+  })
+
+  const onJoin = (channelName: string) => {
+    socket.emit('join_channel', { channelName, channelMember: meAsMember.value })
+  }
+
+  const onVideoSettingsUpdate = (channelName: string) => {
+    socket.emit('update_video_settings', { channelName, channelMember: meAsMember.value })
+  }
+
+  const onExit = (channelName: string) => {
+    socket.emit('exit_channel', { channelName, channelMember: meAsMember.value })
+  }
+
+  socket.on('on_channels_updated', (_channels: Channel[]) => {
+    channels.value = _channels
+  })
+
+  return {
+    channels,
+    joiningChannel,
+
+    onJoin,
+    onVideoSettingsUpdate,
+    onExit
+  }
+}
+
+export type Channels = ReturnType<typeof useChannels>
+
+export const UseChannelsKey: InjectionKey<Channels> = Symbol('UseAuthMe')
