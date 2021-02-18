8e12b9be625a2771246415f0bc97ee7b2e185c8e
minericefield <minericefield@gmail.com>
2021-02-18 22:00:49 +0900

    add delete pending user batch proess

--- server/src/index.dev.ts
+++ server/src/index.dev.ts
@@ -2,6 +2,7 @@ import app from './app'
 
 import { connect } from './db'
 import { init as initChannels } from './services/channels'
+import deletePendingUserBatch from './services/deletePendingUser'
 
 connect(process.env.DB_NAME)
 
@@ -10,6 +11,8 @@ const server = app.listen(3000, ()=>{
 })
 
 initChannels(server)
+
+deletePendingUserBatch()
 
 process.on('uncaughtException', (error) => {
   console.log('uncaughtException', error)
