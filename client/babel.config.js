5eb53281ae6803e296d8d2bf9a0a666c64a3ba3d
minericefield <minericefield@gmail.com>
2021-02-24 22:37:58 +0900

    just incase remove console in production

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+const config = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
 }
+
+if(process.env.NODE_ENV === 'production') {
+  config.plugins = [['transform-remove-console', { exclude: ['error', 'warn'] }]]
+}
+
+module.exports = config
